da49f6972ef3064d5a62bb2e11a120758faad0d5
Kanskii <kz.bk89@gmail.com>
2024-02-08 01:35:09 +0600

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+// import type { NextPage } from "next";
+// import Head from "next/head";
+
+// const Home: NextPage = () => {
+//   return (
+//     <div>
+//       <Head>
+//         <title>Comments Page</title>
+//       </Head>
+//       <div className="p-12">
+//         <h1 className="text-2xl font-bold">Comments!</h1>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>
 }
