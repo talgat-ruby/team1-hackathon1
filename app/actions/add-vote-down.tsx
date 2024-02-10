10b2cdbc5a11dd846122da6034dd4e7f93b7ee2e
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-02-10 11:34:38 +0600

    add: vote fetch
    'use server'
    
    
    export async function addVoterDownServer() {
      console.log('rDown');
    /*
      try {
        const response = await fetch(`ENDPOINT`, {
                method: 'POST',
                body: {},
            });
    
            if(!response.ok){
                throw new Error("Failed to fetch");
              }
            const data = await response.json();
            console.log(data);
    
            return data.data; 
    
    
    } catch (error) {
        console.error('Error post fetch vote down:', error);
      }
      */
      return 'you post vote down'
    }

--- app/actions/add-vote-down.tsx
+++ app/actions/add-vote-down.tsx
@@ -3,7 +3,25 @@
 
 export async function addVoterDownServer() {
   console.log('rDown');
+/*
+  try {
+    const response = await fetch(`ENDPOINT`, {
+            method: 'POST',
+            body: {},
+        });
+
+        if(!response.ok){
+            throw new Error("Failed to fetch");
+          }
+        const data = await response.json();
+        console.log(data);
+
+        return data.data; 
 
 
+} catch (error) {
+    console.error('Error post fetch vote down:', error);
+  }
+  */
   return 'you post vote down'
 }
--- app/actions/add-vote-up.tsx
+++ app/actions/add-vote-up.tsx
@@ -3,6 +3,25 @@
 
 export async function addVoterUPServer() {
   console.log('vote ttt');
+/*
+  try {
+    const response = await fetch(`ENDPOINT`, {
+            method: 'POST',
+            body: {},
+        });
 
-  return 'ttt'
+        if(!response.ok){
+            throw new Error("Failed to fetch");
+          }
+        const data = await response.json();
+        console.log(data);
+
+        return data.data; 
+
+
+} catch (error) {
+    console.error('Error post fetch vote down:', error);
+  }
+  */
+  return 'you post vote up'
 }
